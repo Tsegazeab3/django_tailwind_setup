c97914832a8376fd98e4acf773a78ba4c98630ce
Tsegazeab3 <tsegazeabkebede422@gmail.com>
2025-07-25 13:30:10 +0400

    editted the url.py

--- final_project/app/urls.py
+++ final_project/app/urls.py
@@ -2,10 +2,11 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
+    path("", views.city_portal, name="city_portal"),
     path("home/",views.home, name="home"),
-path("home/city_portal", views.city_portal, name="city_portal"),
-path("home/nav_bar", views.nav_bar, name="nav_bar"),
-path("home/colorful_button", views.colorful_button, name="colorful_button"),
+    path("home/city_portal", views.city_portal, name="city_portal"),
+    path("home/nav_bar", views.nav_bar, name="nav_bar"),
+    path("home/colorful_button", views.colorful_button, name="colorful_button"),
     path("home/apple_website", views.apple_website, name="apple_website")
 ]
 
